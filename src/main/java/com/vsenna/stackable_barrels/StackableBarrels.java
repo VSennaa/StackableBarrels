2b98b4c20e3769c098d95f024cb0602f23ad2ce5
VSenna <viniciosrodrigo.martins@gmail.com>
2026-04-10 16:05:38 -0300

    Bloco TESTE adicionado

--- src/main/java/com/vsenna/stackable_barrels/StackableBarrels.java
+++ src/main/java/com/vsenna/stackable_barrels/StackableBarrels.java
@@ -2,23 +2,31 @@ package com.vsenna.stackable_barrels;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.block.Block;
 
 public class StackableBarrels implements ModInitializer {
 	public static final String MOD_ID = "stackablebarrels";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    public static final Block BARRIL_TESTE = new Block(AbstractBlock.Settings.create().strength(2.0f));
+
+
+    @Override
+	public void onInitialize() {
+        LOGGER.info("Inicializando o mod Stackable Barrels!");
+        Identifier idDoBloco = new Identifier(MOD_ID, "barril_teste");
+        Registry.register(Registries.BLOCK, idDoBloco, BARRIL_TESTE);
+
+        // 3. Registrando o Item do Bloco (para ele poder ir para o inventário do jogador)
+        Registry.register(Registries.ITEM, idDoBloco, new BlockItem(BARRIL_TESTE, new Item.Settings()));
+    }
 }
